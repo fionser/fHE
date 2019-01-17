bf8301ad73c33b847ebca714fd32867347f0e37c
riku <fionser@gmail.com>
2019-01-17 17:48:17 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,31 +7,32 @@
 #include "fHE/encoder.hpp"
 #include "fHE/evaluator.hpp"
 #include "yell/utils/timer.hpp"
+#include "yell/montgomery.hpp"
+#include "fHE/base_converter.hpp"
+#include "fHE/yell.hpp"
+#include <unordered_set>
+
+
 int main() {
-  fHE::SK sk;
+  fHE::SK sk(4);
   fHE::PK pk(sk);
   fHE::MultKey mkey(sk);
-  fHE::RotKey rkey(sk, 1, fHE::RotKey::Direction::RIGHT);
-  fHE::context::poly_t A(fHE::context::nr_ctxt_moduli, yell::uniform{});
-  auto cpy(A);
-  fHE::Cipher ctx;
+  fHE::RotKeySet rkey(sk);
+  fHE::Cipher ctx, ctx0;
   auto encoder = std::make_shared<fHE::Encoder>();
   fHE::Encryptor encryptor(encoder);
   fHE::Decryptor decryptor(encoder);
-  fHE::Evaluator evaluator;
-  std::vector<double> values(fHE::context::degree / 2, 0.123456);
+  fHE::Evaluator evaluator(encoder);
+  std::vector<double> values(fHE::context::degree / 2);
+  for (size_t i = 0; i < values.size(); ++i)
+    values[i] = 1.0 + i;
 
-  std::cout << fHE::context::nr_ctxt_moduli << " moduli with " << yell::params::kModulusBitsize << " bits\n";
+  std::cout << fHE::context::nr_ctxt_moduli << " moduli with " 
+            << yell::params::kModulusBitsize << " bits\n";
   std::cout << fHE::context::degree << " poly degree\n";
   encryptor.encrypt(&ctx, values, pk);
-  double time = 0.;
-  for (int i = 0; i < (1<<7); ++i) {
-    AutoTimer timer(&time);
-    //evaluator.multiply_inplace(&ctx, ctx, mkey);
-    evaluator.rotate_slots_inplace(&ctx, rkey);
-  }
+  evaluator.replicate(&ctx, 4, rkey);
   decryptor.decrypt(&values, ctx, sk);
-
-  std::cout << "time " << time << " " << values[4] << "\n";
+  std::cout << values[4] << "\n";
   return 0;
 }
